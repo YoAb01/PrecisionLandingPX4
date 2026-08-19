e2497e294516030a083e1214a9435440561513f9
YoAb01 <youssef.abl.learn.25@gmail.com>
2026-08-19 20:51:36 +0100

    [detect] Add dectect aruco tag node definition

---
+++ src/drone_vision_landing/src/detect_aruco_tag.cpp
@@ -0,0 +1,28 @@
+#include "geometry_msgs/msg/pose_stamped.hpp"
+#include "sensor_msgs/msg/camera_info.hpp"
+#include "std_msgs/msg/bool.hpp"
+#include <memory>
+#include <chrono>
+#include <functional>
+#include <cmath>
+#include <opencv2/highgui.hpp>
+#include <rclcpp/publisher.hpp>
+#include <rclcpp/subscription.hpp>
+#include <vector>
+#include <string>
+
+#include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/image.hpp>
+#include <sensor_msgs/msg/camera_info.hpp>
+#include <geometry_msgs/msg/pose_stamped.hpp>
+#include <rclcpp/qos.hpp>
+#include <std_msgs/msg/bool.hpp>
+#include <cv_bridge/cv_bridge.hpp>
+
+#include <opencv2/opencv.hpp>
+#include <opencv2/aruco.hpp>
+#include <opencv2/aruco/dictionary.hpp>
+
+int main() {
+  return -1;
+}
